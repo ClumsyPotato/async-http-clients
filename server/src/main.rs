93d873d2df21570f3d6130d8af2cc65dbc48b022
adrian.tomalla <adtoit00@hs-esslingen.de>
2021-06-02 18:34:20 +0200

    adding simple rocket get endpoint

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,14 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+
+#[get("/")]
+fn say_hello() -> &'static str{
+    "Hey, i am the server"
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Starting server");
+    rocket::ignite().mount("/", routes![say_hello]).launch();
 }
